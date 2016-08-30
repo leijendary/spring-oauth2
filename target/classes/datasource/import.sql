3ef72568377f1efdb63cc43b987ee37dd848849d
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2016-08-30 11:53:05 +0800

    removed committed files in gitignore

--- target/classes/datasource/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `oauth2`.`status` (`id`, `code`, `name`) VALUES ('1', 'ACTIVE', 'Active');
-INSERT INTO `oauth2`.`status` (`id`, `code`, `name`) VALUES ('2', 'INACTIVE', 'Inactive');
-INSERT INTO `oauth2`.`status` (`id`, `code`, `name`) VALUES ('3', 'LOCKED', 'Locked');
-INSERT INTO `oauth2`.`status` (`id`, `code`, `name`) VALUES ('4', 'EXPIRED', 'Expired');
